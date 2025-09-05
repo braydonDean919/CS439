bb8ba1eadbf93b51013a78c1c501b24c45378eb6
braydonDean919 <149093650+braydonDean919@users.noreply.github.com>
2025-09-05 14:43:25 -0400

    Added the scoring mechanics and the scoreboard

--- FirstGame/AirHockey.py
+++ FirstGame/AirHockey.py
@@ -2,7 +2,7 @@ import simpleGE, pygame, random
 class Nets(simpleGE.Sprite):
     def __init__(self, scene):
         super().__init__(scene)
-        self.setSize(150, 1)
+        self.setSize(200, 1)
         self.x = self.screenWidth/2
 
 class Puck(simpleGE.Sprite):
@@ -13,10 +13,13 @@ class Puck(simpleGE.Sprite):
         self.boundAction = self.BOUNCE
         self.x = self.screenWidth/2
         self.y = self.screenHeight/2
+        self.rest
         self.setAngle((random.randint(0, 360)))
-
         self.dx = 5
         self.dy = 5
+    def rest(self):
+        self.x = self.screenWidth/2
+        self.y = self.screenHeight/2
 
 class Paddle(simpleGE.Sprite):
     def __init__(self,scene):
@@ -26,6 +29,12 @@ class Paddle(simpleGE.Sprite):
         self.setAngle(90)
         self.x = self.screenWidth/2
         self.boundAction = self.STOP
+class Scorelbl(simpleGE.Label):
+    def __init__(self):
+        super().__init__()
+        self.center = (70, 20)
+        self.text = "Top : Bottom"
+        
 
 
 class Game(simpleGE.Scene):
@@ -42,9 +51,13 @@ class Game(simpleGE.Scene):
         self.netBottom = Nets(self)
         self.netBottom.y = 478
         self.moveSpeed = 4
-        self.sprites = [self.puck, self.paddleBottom,self.paddleTop,self.netBottom,self.netTop]
+        self.topScore = 0
+        self.bottomScore = 0
+        self.scoreLbl = Scorelbl()
+        self.sprites = [self.puck, self.paddleBottom,self.paddleTop,self.netBottom,self.netTop,self.scoreLbl]
     def process(self):
         maxDY = 5
+        self.scoreLbl.text = f"{self.topScore} : {self.bottomScore}"
         if self.isKeyPressed(pygame.K_d):   
             self.paddleTop.x += self.moveSpeed
         if self.isKeyPressed(pygame.K_a):   
@@ -76,6 +89,12 @@ class Game(simpleGE.Scene):
             relY *= 2
             relY *=maxDY
             self.puck.dy = relY
+        if self.puck.collidesWith(self.netBottom):
+            self.puck.rest()
+            self.topScore +=1
+        if self.puck.collidesWith(self.netTop):
+            self.puck.rest()
+            self.bottomScore +=1
 
 
 
